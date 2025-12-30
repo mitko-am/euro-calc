e3bbcc99144dfe27b12b2844dd33caee82170268
mitko-am <mitko.am@gmail.com>
2025-12-30 12:34:02 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'v1_euro_calc';
+const assets = ['./', './index.html', './styles.css', './script.js', './manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
